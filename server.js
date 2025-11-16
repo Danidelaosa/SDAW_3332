62dcb9d98261c0573b81effeb0b8a2a08f5e9a29
Daniela Carrillo <1593332@alu.murciaeduca.es>
2025-11-16 12:55:34 +0100

    Actualizado server.js para cargar index.html correctamente

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname));
-
 const PORT = 3000;
+
+// Ruta para el index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en http://localhost:${PORT}`);
